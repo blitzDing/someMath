3fd3492d5caed32902751b4f0d409a974dfb5049
blitzDing <johnbernard@hotmail.de>
2025-02-24 14:50:43 +0100

    Partitions on Solide Ground.

--- src/main/java/someMath/Partitions.java
+++ src/main/java/someMath/Partitions.java
@@ -5,20 +5,19 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import someMath.exceptions.MathException;
+
 public class Partitions
 {
 
-	public static Set<List<Integer>> summandsBiggerSet(int minSizeOfSummand, int nrOfSummands, int sum)
+	public static Set<List<Integer>> summandsBiggerSet(int minSizeOfSummand, int nrOfSummands, int sum) throws MathException
 	{
 
 		Set<List<Integer>> output = new HashSet<>();
 		
 		if(minSizeOfSummand*nrOfSummands>sum)return output;
 
-		if(sum==0)return output;
- 
-		if(nrOfSummands==0)return output;
-
+		if(minSizeOfSummand<=0||nrOfSummands<=0||sum<=0)throw new MathException("At least one of the Arguments is Zero or Below.");
 		if(sum==1)
 		{
 			List<Integer> list = new ArrayList<>();
@@ -39,10 +38,7 @@ public class Partitions
 		for(int i=minSizeOfSummand;i<sum;i++)
 		{
 			
-			if(i*(nrOfSummands-1)>sum)continue;
-			
 			Set<List<Integer>> set =summandsBiggerSet(i, nrOfSummands-1, sum-i);
-			if(set.isEmpty())continue;
 			
 			for(List<Integer> listRight: set)
 			{
@@ -51,29 +47,13 @@ public class Partitions
 				list.addAll(listRight);
 				output.add(list);
 			}
-			
-			
 		}
 
 		return output;
 	}
 	
-	public static Set<List<Integer>>summandsOfAnySizeFixedSummandNr(int nrOfSummands, int sum)
-	{
-		
-		
-		Set<List<Integer>> output = new HashSet<>();
-		if(nrOfSummands>sum)return output;
-		
-		for(int j=1;j<sum;j++)
-		{
-			output.addAll(summandsBiggerSet(j, nrOfSummands, sum));
-		}
-		
-		return output;
-	}
 	
-	public static Set<List<Integer>> partionsOfNAsLists(int sum)
+	public static Set<List<Integer>> partionsOfNAsLists(int sum) throws MathException
 	{
 		Set<List<Integer>> output = new HashSet<>();
 		
@@ -81,9 +61,24 @@ public class Partitions
 		
 		for(int e=1;e<sum;e++)
 		{
-			output.addAll(summandsOfAnySizeFixedSummandNr(e, sum));
+			output.addAll(summandsBiggerSet(1, e, sum));
 		}
+		
+		List<Integer> bunchOfOnes = new ArrayList<>();
+		for(int n=0;n<sum;n++)bunchOfOnes.add(1);
+		output.add(bunchOfOnes);
+		
 		return output;
 
+	}
+	
+	public static int sumOfListEntries(List<Integer> list)
+	{
+		int sum = 0;
+		for(int n=0;n<list.size();n++)
+		{
+			sum = sum + list.get(n);
+		}
+		return sum;
 	}
 }
--- src/test/java/someMath/PartionsTests.java
+++ src/test/java/someMath/PartionsTests.java
@@ -7,19 +7,45 @@ import org.junit.jupiter.api.Test;
 
 import consoleTools.TerminalXDisplay;
 import someMath.exceptions.CollectionException;
+import someMath.exceptions.MathException;
 
 public class PartionsTests
 {
 
 	@Test
-	public void summandsBiggerTest() throws CollectionException
+	public void summandsBiggerTest() throws CollectionException, MathException
 	{
 		
 		int minSize = 1;
-		int nrOfSummands = 4;
+		int nrOfSummands = 8;
 		int sum = 10;
 		Set<List<Integer>> set =Partitions.summandsBiggerSet(minSize, nrOfSummands, sum);
+		System.out.println("Min Size: " + minSize);
+		System.out.println("Nr of Summands: " + nrOfSummands);
+		System.out.println("Sum: " + sum + "\n");
+		System.out.println(set + "\n");
 		
+		for(List<Integer> list: set)
+		{
+			assert(list.size()==nrOfSummands);
+			assert(Partitions.sumOfListEntries(list)==sum);
+		}
+		
+		sum = 6;
+		set = Partitions.partionsOfNAsLists(sum);
+		int p = set.size();
+		
+		System.out.println("Min Size: " + minSize);
+		System.out.println("Nr of Summands: " + nrOfSummands);
+		System.out.println("Sum: " + sum);
+		System.out.println("Partitions: " + p + "\n");
 		System.out.println(set);
+		
+		for(List<Integer> list: set)
+		{
+			assert(Partitions.sumOfListEntries(list)==sum);
+		}
+
+
 	}
 }
